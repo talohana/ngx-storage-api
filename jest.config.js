f22172817b0e2b21512340a52cf076b93748afcc
talohana <tal.ohana.x@gmail.com>
2020-09-11 21:11:52 +0300

    fix(jest): added relevant configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/src/setupJest.ts"],
+  preset: 'jest-preset-angular',
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  setupFilesAfterEnv: ['<rootDir>/test.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
 };
--- projects/ngx-storage/src/lib/local-storage.service.ts
+++ projects/ngx-storage/src/lib/local-storage.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class NgxStorageService {
+export class LocalStorageService {
 
   constructor() { }
 }
