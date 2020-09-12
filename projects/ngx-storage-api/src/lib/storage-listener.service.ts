e3feb4f437f2d68133bb189399eecf6cd081d512
talohana <tal.ohana.x@gmail.com>
2020-09-12 19:46:28 +0300

    fix(storage-listener): shared StorageEvents instead of replaying

--- projects/ngx-storage-api/src/lib/storage-listener.service.ts
+++ projects/ngx-storage-api/src/lib/storage-listener.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { defer, fromEvent, Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { share } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageListenerService {
+  /**
+   * Listen to window storage events.
+   *
+   * @returns
+   * An Observable which listens to window storage events,
+   * shared to add a single handler to window object
+   */
   readonly storage$: Observable<StorageEvent> = defer(() =>
-    fromEvent<StorageEvent>(window, 'storage').pipe(shareReplay(1))
+    fromEvent<StorageEvent>(window, 'storage').pipe(share())
   );
 }
