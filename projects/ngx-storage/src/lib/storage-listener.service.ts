17b43c24b316dfbd91e8c8da3580fd27672fdfd6
talohana <tal.ohana.x@gmail.com>
2020-09-12 13:08:27 +0300

    test(storage-listener): added test using manual mocks

--- projects/ngx-storage/src/lib/storage-listener.service.ts
+++ projects/ngx-storage/src/lib/storage-listener.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
+import { defer, fromEvent, Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageListenerService {
-  readonly storage$: Observable<StorageEvent> = fromEvent<StorageEvent>(
-    window,
-    'storage'
-  ).pipe(shareReplay(1));
+  readonly storage$: Observable<StorageEvent> = defer(() =>
+    fromEvent<StorageEvent>(window, 'storage').pipe(shareReplay(1))
+  );
 }
--- projects/ngx-storage/src/lib/tests/storage-listener.service.spec.ts
+++ projects/ngx-storage/src/lib/tests/storage-listener.service.spec.ts
@@ -1,5 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { hot } from 'jest-marbles';
+import { when } from 'jest-when';
+import { EMPTY, fromEvent } from 'rxjs';
 import { StorageListenerService } from '../storage-listener.service';
+
+jest.mock('rxjs', () => {
+  const observables = jest.requireActual('rxjs');
+
+  observables.fromEvent = jest.fn(() => EMPTY);
+
+  return observables;
+});
 
 describe('StorageListenerService', () => {
   let service: StorageListenerService;
@@ -7,13 +18,19 @@ describe('StorageListenerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StorageListenerService);
+
+    jest.resetModules();
+    jest.resetAllMocks();
   });
 
   it('should listen for window storage events', () => {
-    fail();
-  });
+    const event = new StorageEvent('storage');
+    const eventObs = hot('e', { e: event });
 
-  it('should keep single subscription to storage events', () => {
-    fail();
+    when(fromEvent as jest.Mock)
+      .calledWith(window, 'storage')
+      .mockReturnValueOnce(eventObs);
+
+    expect(service.storage$).toBeObservable(eventObs);
   });
 });
